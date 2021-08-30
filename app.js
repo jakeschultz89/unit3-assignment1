3dfb2ddc3fd7d6e557ba2651efd25c46e5a00705
jakeschultz89 <ejacobschultz@gmail.com>
2021-08-30 08:43:25 -0700

    added 20 variables

---
+++ app.js
@@ -0,0 +1,44 @@
+var myName = "Jake";
+var myAge = 31;
+var myWife = "Sarah";
+var myKid1 = "Max";
+var myKid2 = "Sawyer";
+var myKid3 = "Caleb";
+var myDog = "Archie";
+var myJob = "student";
+var myHome = "37 foot travel trailer";
+var myTruck = "F-250"
+var myLocation = "all over the USA";
+var myKids = 3;
+var myShow = "Doctor Who";
+var myMovie = "Interstellar";
+var myBook = "The Martian";
+var myHobby1 = "hiking";
+var myHobby2 = "kayaking";
+var myHobby3 = "video games";
+var myFood = "pizza";
+var myDrink = "cranberry juice";
+
+var assignment = `
+    <p> Name: ${myName} </p>
+    <p> Age: ${myAge} </p>
+    <p> Wife: ${myWife} </p>
+    <p> Number of Kids: ${myKids} </p>
+    <p> Oldest Son: ${myKid1} </p>
+    <p> Middle Son: ${myKid2} </p>
+    <p> Youngest Son: ${myKid3} </p>
+    <p> Dog: ${myDog} </p>
+    <p> Occupation: ${myJob} </p>
+    <p> My Home: ${myHome} </p>
+    <p> My Location: ${myLocation} </p>
+    <p> My Truck: ${myTruck} </p>
+    <p> Favorite TV Show: ${myShow} </p>
+    <p> Favorite Movie: ${myMovie} </p>
+    <p> Favorite Book: ${myBook}</p>
+    <p> Favorite Hobby 1: ${myHobby1} </p>
+    <p> Favorite Hobby 2: ${myHobby2} </p>
+    <p> Favorite Hobby 3: ${myHobby3} </p>
+    <p> Favorite Food: ${myFood} </p>
+    <p> Favorite Drink: ${myDrink} </p>
+`
+document.write(assignment);
